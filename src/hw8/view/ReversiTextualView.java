3ff5ebe963cd30757ce4daf7904fb4fa7a0b9979
Vince <li.vin@northeastern.edu>
2023-12-05 14:54:59 -0500

    first commit

---
+++ src/hw8/view/ReversiTextualView.java
@@ -0,0 +1,13 @@
+package hw8.view;
+
+/**
+ * Interface for the text view of a reversi game.
+ */
+public interface ReversiTextualView {
+
+  /**
+   * Renders a board.
+   * @return the formatted game
+   */
+  String toString();
+}
